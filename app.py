e8f4993db54d1a968eda72c61d52669086b263dc
JaspinderKaurWalia26 <132120070+JaspinderKaurWalia26@users.noreply.github.com>
2026-01-14 12:35:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,8 @@ stud1=Student("Anshul")
 print(stud1.name)
 print("Hello")
 print("Good evening")
-
-
-#python
-
-#message
-
-#test
-#new
-
-print("good afternoon")
-
-
-print("new")
-
-
 print("Good  Morning")
-print("testing")
+
 
 
 
